47168006a81fc883f205c4f3895e45e944bb426d
Anik Doshi <asdoshi@hotmail.com>
2022-11-04 13:20:07 +0530

    commit

--- SocialShout-v2.sql
+++ SocialShout-v2.sql
@@ -2,9 +2,9 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Sep 13, 2022 at 08:28 PM
--- Server version: 10.4.21-MariaDB
+-- Host: 127.0.0.1
+-- Generation Time: Nov 04, 2022 at 08:49 AM
+-- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `SocialShout`
+-- Database: `socialshout`
 --
 
 -- --------------------------------------------------------
@@ -35,10 +35,19 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` text NOT NULL,
-  `profile_pic` int(11) NOT NULL,
+  `profile_pic` text NOT NULL DEFAULT '\'default_profile.jpg\'',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `acc_status` int(11) NOT NULL COMMENT '0- Not Verified,1- Active,2- blocked '
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `gender`, `email`, `username`, `password`, `profile_pic`, `created_at`, `updated_at`, `acc_status`) VALUES
+(1, 'Anik', 'Doshi', 1, 'anikdoshi2003@gmail.com', 'test1', 'cc03e747a6afbbcbf8be7668acfebee5', '1667547884profile.jpg', '2022-11-01 15:14:46', '2022-11-04 07:44:44', 1),
+(11, 'test2', 'test', 1, 'anikdoshi3@gmail.com', 'test2', 'cc03e747a6afbbcbf8be7668acfebee5', 'default_profile.jpg', '2022-11-02 13:04:46', '2022-11-04 07:41:59', 1);
 
 --
 -- Indexes for dumped tables
@@ -58,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
