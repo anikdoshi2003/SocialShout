d092a7c21915ed80ab3f8da78f47386553353c80
Anik Doshi <asdoshi@hotmail.com>
2022-11-05 19:21:23 +0530

    commit

--- SocialShout-v3.sql
+++ SocialShout-v3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2022 at 08:49 AM
+-- Generation Time: Nov 05, 2022 at 02:48 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `socialshout`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_img` text NOT NULL,
+  `post_text` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `post_img`, `post_text`, `created_at`) VALUES
+(1, 1, '1667634366Social Shout-1-2.png', 'first image', '2022-11-05 07:46:06'),
+(3, 1, '1667636520SocialShout-logo-1.png', '2nd post', '2022-11-05 08:22:00'),
+(4, 1, '1667636802SocialShout-logo-2.jpeg', '', '2022-11-05 08:26:42');
 
 -- --------------------------------------------------------
 
@@ -46,12 +69,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `gender`, `email`, `username`, `password`, `profile_pic`, `created_at`, `updated_at`, `acc_status`) VALUES
-(1, 'Anik', 'Doshi', 1, 'anikdoshi2003@gmail.com', 'test1', 'cc03e747a6afbbcbf8be7668acfebee5', '1667547884profile.jpg', '2022-11-01 15:14:46', '2022-11-04 07:44:44', 1),
+(1, 'Anik', 'Doshi', 1, 'anikdoshi2003@gmail.com', 'test1', 'cc03e747a6afbbcbf8be7668acfebee5', '1667636220ANIKDOSHI1809PHOTO.jpg', '2022-11-01 15:14:46', '2022-11-05 08:17:00', 1),
 (11, 'test2', 'test', 1, 'anikdoshi3@gmail.com', 'test2', 'cc03e747a6afbbcbf8be7668acfebee5', 'default_profile.jpg', '2022-11-02 13:04:46', '2022-11-04 07:41:59', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -62,6 +91,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
