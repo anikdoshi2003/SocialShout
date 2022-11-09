3cf5267e34b5973892845fe1ee6fb83f3017e5cf
Anik Doshi <asdoshi@hotmail.com>
2022-11-09 15:47:54 +0530

    Update SocialShout-v4.sql

--- SocialShout-v4.sql
+++ SocialShout-v4.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2022 at 10:46 AM
+-- Generation Time: Nov 09, 2022 at 11:15 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -26,24 +26,103 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `follow_list`
 --
--- Error reading structure for table socialshout.follow_list: #1932 - Table &#039;socialshout.follow_list&#039; doesn&#039;t exist in engine
--- Error reading data for table socialshout.follow_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `socialshout`.`follow_list`&#039; at line 1
+
+CREATE TABLE `follow_list` (
+  `id` int(11) NOT NULL,
+  `follower_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `follow_list`
+--
+
+INSERT INTO `follow_list` (`id`, `follower_id`, `user_id`) VALUES
+(0, 1, 11);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `posts`
 --
--- Error reading structure for table socialshout.posts: #1932 - Table &#039;socialshout.posts&#039; doesn&#039;t exist in engine
--- Error reading data for table socialshout.posts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `socialshout`.`posts`&#039; at line 1
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_img` text NOT NULL,
+  `post_text` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `post_img`, `post_text`, `created_at`) VALUES
+(1, 1, '1667634366Social Shout-1-2.png', 'first image', '2022-11-05 07:46:06'),
+(3, 1, '1667636520SocialShout-logo-1.png', '2nd post', '2022-11-05 08:22:00'),
+(4, 1, '1667636802SocialShout-logo-2.jpeg', '', '2022-11-05 08:26:42'),
+(5, 11, '1667988936SocialShout-logo-1.png', '', '2022-11-09 10:15:36');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
--- Error reading structure for table socialshout.users: #1932 - Table &#039;socialshout.users&#039; doesn&#039;t exist in engine
--- Error reading data for table socialshout.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `socialshout`.`users`&#039; at line 1
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `gender` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` text NOT NULL,
+  `profile_pic` text NOT NULL DEFAULT '\'default_profile.jpg\'',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `acc_status` int(11) NOT NULL COMMENT '0- Not Verified,1- Active,2- blocked '
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `gender`, `email`, `username`, `password`, `profile_pic`, `created_at`, `updated_at`, `acc_status`) VALUES
+(1, 'Anik', 'Doshi', 1, 'anikdoshi2003@gmail.com', 'anik2003', 'cc03e747a6afbbcbf8be7668acfebee5', '1667636220ANIKDOSHI1809PHOTO.jpg', '2022-11-01 15:14:46', '2022-11-09 10:14:49', 1),
+(11, 'test2', 'test', 1, 'anikdoshi3@gmail.com', 'test2', 'cc03e747a6afbbcbf8be7668acfebee5', 'default_profile.jpg', '2022-11-02 13:04:46', '2022-11-04 07:41:59', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
